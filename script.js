6aa46fabbc8aa0e687592c891a4c894633fdbba3
ManyaPahuja <wcscdwmanya@gmail.com>
2021-09-26 18:56:09 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+
+     function(const.let.button){
+
+     button=
+     function(document,querySelector).document;
+
+    // function.const(button.document.function);
+     document.const(button.document.function);
+
+     document.const(button.floor.document);
+
+     floor.addEventListener(floor.function){
+     
+        const.button(document.const.button)
+        onclick( floor.document.button)
+
+
+        function(random.floor.onclick.button)
+         document.function(floor.document)
+            function(onclick.floor.random)
+            const.button(document.button.function);
+
+            button.document(random.onclick.bind);
+            button.random( function.floor.random)
+     }
+     
+}
